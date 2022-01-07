95d535ea54b62728f13629fed5f51ec832ba8ab1
Dumrong <dumdumrong13@gmail.com>
2022-01-07 12:03:48 +0700

    Update

--- include/headertop.php
+++ include/headertop.php
@@ -1,17 +1,17 @@
-<div class="container shadow-lg my-3 p-2 rounded">
+<div class="container shadow-lg my-3 p-3 rounded">
 	<div class="row">
 
-		<div class="col-md-1 my-20">
+		<div class="col-md-1 my-20 ">
 			<div style="width: 13rem;">
-				<img src="images/slider/header1.jpg" class="card-img-top" alt="">
+				<img src="images/slider/header1.jpg" class="card-img-top rounded" alt="">
 			</div>
 		</div>
 
 		<div class="col-md-11">
 			<div style="width: 50rem;">
-				<h4 class="ml-auto mt-4">วิทยาลัยเทคนิคตราด</h4>
-				<div class="underline"></div>
-				<h4 class="ml-auto">Trat Techical College</h4>
+				<h4 class="mt-4" style="margin-left: 7rem;">วิทยาลัยเทคนิคตราด</h4>
+				<div class="underline" style="margin-left: 7rem;"></div>
+				<h4 class="ml-auto" style="margin-left: 7rem;">Trat Techical College</h4>
 			</div>
 		</div>
 
